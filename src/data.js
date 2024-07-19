4d8ec3d446d45b7ecee67e5dd0eeb3927514f7f3
TakahiroFujitaSJB <TakahiroTanakaSJB@gmail.com>
2024-07-19 15:43:06 -0600

    finished and added css and data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const inventoryData = [
+  { name: `'Ao No Sumika' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/cLgCwqVp/Ao-No-Sumika.jpg' },
+  { name: `'Going On' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/FHMRxWv7/GoingOn.jpg' },
+  { name: `'Hunger' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/ZnR5VXMz/290009114-570207314706797-8964377742502211485-n.jpg' },
+  { name: `'Literary Nonsense' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/6QtwMD8b/literary-nonsense.jpg' },
+  { name: `'Demon Dance Tokyo' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/VL2kWG92/291498510-398676722232454-3361050522912371738-n.png' },
+  { name: `'Mabel' Full Album + Cover`, price: 6.99, img: 'https://i.postimg.cc/tJMCfDKZ/Capture.jpg' },
+  { name: `'Yamiyo' Full Album + Cover`, price: 10.99, img: 'https://i.postimg.cc/2ybxLjf1/290191547-356792736589291-56584295225431517-n.jpg' },
+  { name: `'Eve' Special Music Box Collection`, price: 10.99, img: 'https://i.postimg.cc/qMN7nVK8/musicbox.jpg' },
+  
+
+
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+  { name: 'Place Holder', price: 0.00, img: 'https://i.postimg.cc/G3Y7p97Y/placeholder.jpg' },
+];
+
+export const currenciesData = ['USD', 'EUR', 'CAD'];
